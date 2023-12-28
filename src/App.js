f8554754ab63254a7c63277179b78b79edd1c2ee
lilshebeary <cirijaye@gmail.com>
2023-12-28 04:05:37 -0800

    add React Router, Browser Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import Header from './components/Header';
-import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
-import Footer from './components/Footer';
-import './App.css';
-
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ContactPage from "./pages/ContactPage";
+import Header from "./components/Header";
+import CampsitesDirectoryPage from "./pages/CampsitesDirectoryPage";
+import Footer from "./components/Footer";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <CampsitesDirectoryPage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="contact" element={<ContactPage />} />
+        <Route path="directory" element={<CampsitesDirectoryPage />} />
+      </Routes>
       <Footer />
     </div>
   );
